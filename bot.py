67b45523bb26a65e071bad96fca455aee420f045
del <ept.soul@gmail.com>
2024-09-30 22:30:41 +0300

    Add comments

--- bot.py
+++ bot.py
@@ -6,22 +6,25 @@ import time
 
 child_handles = []
 
-
+#Mute sound
 def sound_off(): 
     lParam = win32api.MAKELONG(35, 450)
     win32api.SendMessage(child_handles[-3], win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
     win32api.SendMessage(child_handles[-3], win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, lParam)
 
+#Finder telegram id
 def hwnd_finder():
     hWnd = win32gui.FindWindow(None, "TelegramDesktop")
     return hWnd
 
+#Finder telegram app
 def all_ok(hwnd, param):
     child_handles.append(hwnd)
 
+#Clicker
 def clicker(x,y):
     lParam = win32api.MAKELONG(x, y) #position of the clicker
-    #clicker
+    
     win32api.SendMessage(child_handles[-3], win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
     win32api.SendMessage(child_handles[-3], win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, lParam)
     time.sleep(0.01)
